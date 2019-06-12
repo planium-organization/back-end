da86739432e4d4cbc41b426600beeb39f044f2a5
yegmor <yegmor@gmail.com>
2019-06-12 20:02:01 +0430

    Add comment controller for supervisor

---
+++ src/KP.BackEnd/Areas/Supervisor/Controllers/CommentController.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using KP.BackEnd.Core;
+using KP.BackEnd.Core.DTOs.Shared.Comment;
+using KP.BackEnd.Core.DTOs.Supervisor.Comment;
+using KP.BackEnd.Core.Models;
+using KP.BackEnd.Persistence.EntityConfigurations;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KP.BackEnd.Areas.Supervisor.Controllers
+{
+    // [Authorize]
+    [Route("api/supervisor/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+         
+        public CommentController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{studentId}/{date}/{page}/{count}")]
+        public async Task<ActionResult<IEnumerable<CommentGetDto>>> GetAll(Guid studentId, DateTime date, int page, int count)
+        {
+            var userId= ApplicationUserConfiguration.SupervisorIdTmp;
+            var comments = await _unitOfWork.Comments.GetRange(userId ,studentId, date, page, count);
+
+            var commentDtos = comments.Select(c => _mapper.Map<CommentGetDto>(c));
+            
+            return Ok(commentDtos);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CommentGetDto>> Create([FromBody]CommentPostDto commentDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var userId = ApplicationUserConfiguration.SupervisorIdTmp;
+            
+            var comment = _mapper.Map<Comment>(commentDto);
+            comment.CreationTime = DateTime.Now;
+            comment.SupervisorId = userId;
+
+            await _unitOfWork.Comments.Add(comment);
+            await _unitOfWork.Complete();
+
+            var commentGetDto = _mapper.Map<CommentGetDto>(comment);
+            
+            return Ok(commentGetDto);
+        }
+//        [HttpGet("{id}")]
+//        public async Task<ActionResult<CommentGetDto>> Get(Guid id)
+//        {
+//            var userId = ApplicationUserConfiguration.SupervisorIdTmp;
+//            var comment = await _unitOfWork.Comments.Find(userId, id);
+//            if (comment == null)
+//                return NotFound("card not found");
+//            
+//            var commentDto = _mapper.Map<CommentGetDto>(comment); // TODO
+//            return Ok(commentDto);
+//        }
+    }
+}
