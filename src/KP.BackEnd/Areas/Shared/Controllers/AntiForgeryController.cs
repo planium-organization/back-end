04eb34a167fd5bc0c001ce205b31bece19284b2f
M. M. Abdollahpour <mohammadmahdiabdollahpour@gmail.com>
2019-07-04 14:50:53 +0430

    Add AntiForgery Controller

---
+++ src/KP.BackEnd/Areas/Shared/Controllers/AntiForgeryController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KP.BackEnd.Areas.Shared.Controllers
+{
+    [ApiController]
+    public class AntiForgeryController : Controller
+    {
+        private readonly IAntiforgery _antiForgery;
+        public AntiForgeryController(IAntiforgery antiForgery)
+        {
+            _antiForgery = antiForgery;
+        }
+        [Route("api/antiforgery")]
+        [IgnoreAntiforgeryToken]
+        public IActionResult Index()
+        {
+            var tokens = _antiForgery.GetAndStoreTokens(HttpContext);
+            Response.Cookies.Append("XSRF-REQUEST-TOKEN", tokens.RequestToken, new Microsoft.AspNetCore.Http.CookieOptions
+            {
+                HttpOnly = false
+            });
+            return NoContent();
+        }
+    }
+}
