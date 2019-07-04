be04f049b94fab50530afbf84ebfacbc39f23b2a
yegmor <yegmor@gmail.com>
2019-07-04 16:06:57 +0430

    Add channelpost controller for student and supervisor

---
+++ src/KP.BackEnd/Areas/Supervisor/Controllers/ChannelPostController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using KP.BackEnd.Core;
+using KP.BackEnd.Core.DTOs.Shared.ChannelPost;
+using KP.BackEnd.Core.DTOs.Supervisor.ChannelPost;
+using KP.BackEnd.Core.Models;
+using KP.BackEnd.Persistence.EntityConfigurations;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KP.BackEnd.Areas.Supervisor.Controllers
+{
+    // [Authorize]
+    [Route("api/supervisor/[controller]")]
+    [ApiController]
+    public class ChannelPostController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        
+        public ChannelPostController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ChannelPostGetDto>> Get(Guid id)
+        {
+            var userId = ApplicationUserConfiguration.SupervisorIdTmp;
+            var channelPost = await _unitOfWork.ChannelPosts.Find(userId, id);
+            return Ok(_mapper.Map<ChannelPostGetDto>(channelPost)); // TODO
+        }
+        
+        [HttpGet("{page}/{count}")]
+        public async Task<ActionResult<IEnumerable<ChannelPostGetDto>>> GetAll(Guid classId, int page, int count)
+        {
+            var channelPosts = await _unitOfWork.ChannelPosts.GetRange(classId,page, count);
+            var channelPostDtos = channelPosts.Select(cp => _mapper.Map<ChannelPostGetDto>(cp)); // TODO
+            return Ok(channelPostDtos);
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult<ChannelPostGetDto>> Create([FromBody] ChannelPostPostDto dto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var userId = ApplicationUserConfiguration.SupervisorIdTmp;
+            var channelPost = _mapper.Map<ChannelPost>(dto); // TODO
+
+            await _unitOfWork.ChannelPosts.Add(channelPost);
+            await _unitOfWork.Complete();
+            
+            return CreatedAtAction(nameof(Get), _mapper.Map<ChannelPostGetDto>(channelPost), null);
+        }
+    }
+}
