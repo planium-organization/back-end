5be12e50917f72c122e9ec96f58379a66d639617
yegmor <yegmor@gmail.com>
2019-07-04 15:25:57 +0430

    modify repo, Irepo, unitofwork, Iunitofwork for schoolclass

--- src/KP.BackEnd/Core/Repositories/ISchoolClassRepository.cs
+++ src/KP.BackEnd/Core/Repositories/ISchoolClassRepository.cs
@@ -1,10 +1,12 @@
+using System;
 using System.Threading.Tasks;
 using KP.BackEnd.Core.Models;
 
-namespace KP.BackEnd.Persistence.Repositories
+namespace KP.BackEnd.Core.Repositories
 {
     public interface ISchoolClassRepository
     {
+        Task<SchoolClass> Find(Guid id);
         Task Add(SchoolClass schoolClass);
     }
 }
