f350132677b2dc28d361a704d8f53ed7eb881582
M. M. Abdollahpour <mohammadmahdiabdollahpour@gmail.com>
2019-07-09 03:41:56 +0430

    Add edit course endpoint

--- src/KP.BackEnd/Areas/Student/Controllers/CourseController.cs
+++ src/KP.BackEnd/Areas/Student/Controllers/CourseController.cs
@@ -55,6 +55,24 @@ namespace KP.BackEnd.Areas.Student.Controllers
             return Ok(courses);
         }
 
-        
+        [HttpPut("{id}")]
+        public async Task<ActionResult<CourseGetDto>> Edit(Guid id, [FromBody] CoursePutDto putDto)
+        {
+            var userId = Guid.Parse(_userManager.GetUserId(User));
+            var student = await _unitOfWork.Students.Find(userId);
+            var course = student.Courses.FirstOrDefault(c => c.Id == id);
+            if (course == null)
+                return BadRequest("Course not found");
+
+            if (putDto.Title != null)
+                course.Title = putDto.Title;
+            
+            if (putDto.Color != null)
+                course.Color = putDto.Color;
+
+            await _unitOfWork.Complete();
+
+            return Ok(_mapper.Map<CourseGetDto>(course));
+        }
     }
 }
