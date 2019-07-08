c275b7e057438544dbe998e01cbfcde4917950dc
yegmor <yegmor@gmail.com>
2019-07-08 20:42:12 +0430

    Merge dev into fb_schoolclass

--- src/KP.BackEnd/Core/AutoMapperProfiles/SchoolClassProfile.cs
+++ src/KP.BackEnd/Core/AutoMapperProfiles/SchoolClassProfile.cs
@@ -1,6 +1,6 @@
 using AutoMapper;
 using KP.BackEnd.Core.DTOs.Shared;
-using KP.BackEnd.Core.DTOs.Shared.SchoolClass;
+using KP.BackEnd.Core.DTOs.Supervisor.SchoolClass;
 using KP.BackEnd.Core.Models;
 
 namespace KP.BackEnd.Core.AutoMapperProfiles
