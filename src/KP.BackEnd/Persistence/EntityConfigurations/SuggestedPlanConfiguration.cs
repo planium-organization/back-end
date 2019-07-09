ee62fea92b162dbe11409883b4e6438c678a366e
M. M. Abdollahpour <mohammadmahdiabdollahpour@gmail.com>
2019-07-09 22:50:43 +0430

    Add suggested plan configuration

---
+++ src/KP.BackEnd/Persistence/EntityConfigurations/SuggestedPlanConfiguration.cs
@@ -0,0 +1,15 @@
+using KP.BackEnd.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace KP.BackEnd.Persistence.EntityConfigurations
+{
+    public class SuggestedPlanConfiguration : IEntityTypeConfiguration<SuggestedPlan>
+    {
+        public void Configure(EntityTypeBuilder<SuggestedPlan> builder)
+        {
+            builder.HasOne(sp => sp.SchoolClass)
+                .WithOne(sc => sc.SuggestedPlan);
+        }
+    }
+}
