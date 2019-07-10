4ccc892921ee892cfc6a97d350606b91cf5f0af5
yegmor <yegmor@gmail.com>
2019-07-10 22:31:26 +0430

    Add edit card to modify suggested plan

--- src/KP.BackEnd/Areas/Supervisor/Controllers/SuggestedPlanController.cs
+++ src/KP.BackEnd/Areas/Supervisor/Controllers/SuggestedPlanController.cs
@@ -62,6 +62,62 @@ namespace KP.BackEnd.Areas.Supervisor.Controllers
 
             return Ok(_mapper.Map<SuggestedPlanGetDto>(plan));
         }
+
         
+        
+        
+        [HttpPut("{classId}/{cardId}")]
+        public async Task<ActionResult> EditCard(Guid classId, Guid cardId,[FromBody] CardPutDto cardPut)
+        {
+            var userId = Guid.Parse(_userManager.GetUserId(User));
+            var supervisor = await _unitOfWork.Supervisors.Find(userId);
+
+            var sClass = supervisor.SchoolClasses.FirstOrDefault(c => c.Id == classId);
+            if (sClass == null)
+                return BadRequest("Class Id is not valid");
+
+            var card = sClass.SuggestedPlan.Cards.FirstOrDefault(c=>c.Id == cardId);
+            if (card == null)
+                return NotFound("card not found");
+
+            
+            bool expired = card.StartTime == null
+                ? (DateTime.Today - card.DueDate.Date).Days >= 1
+                : (card.StartTime + card.Duration) < DateTime.Now;
+            bool editable = !expired && card.Status != CardStatus.Done;
+            if (!editable)
+                return BadRequest("card isn't editable!!");
+
+            if (cardPut.StartTime.HasValue)
+                card.StartTime = cardPut.StartTime.Value;
+
+            if (cardPut.Duration.HasValue)
+                card.Duration = cardPut.Duration.Value;
+
+            if (cardPut.DueDate.HasValue)
+                card.DueDate = cardPut.DueDate.Value;
+
+            if (cardPut.Course != null)
+                card.Course = _mapper.Map<Course>(cardPut.Course);
+
+            if (cardPut.Description != null)
+                card.Description = cardPut.Description;
+
+            await _unitOfWork.Complete();
+
+
+            var cardGetDto = _mapper.Map<CardGetDto>(card);
+
+            cardGetDto.Done = card.Status == CardStatus.Done;
+            cardGetDto.SupervisorCreated = card.SupervisorId.HasValue;
+            cardGetDto.Expired = cardGetDto.StartTime == null
+                ? (DateTime.Today - cardGetDto.DueDate.Date).Days >= 1
+                : (cardGetDto.StartTime + cardGetDto.Duration) < DateTime.Now;
+            cardGetDto.Editable = !cardGetDto.Expired && card.Status != CardStatus.Done;
+
+            return Ok(cardGetDto);
+        }
+        
+       
     }
 }
